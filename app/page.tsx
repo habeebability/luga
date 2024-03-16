cac1302c5d1d761c800e80ce477aca680f3ecf35
Habeebability <awoyemi.habeeb@gmail.com>
2024-03-16 23:19:19 +0000

    add button library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-green-600">Hello</p>
+       <Button>Click me</Button>
     </div>
   );
 }
