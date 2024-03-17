f38ae1b80937711d10a942638776673321553e6f
Habeebability <awoyemi.habeeb@gmail.com>
2024-03-17 01:47:49 +0000

    add sidebar,mobile header and main layout with learn

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,23 @@
+import { Sidebar } from "@/components/Sidebar"
+import { MobileHeader } from "@/components/mobile-header"
+
+
+
 type Props = {
     children: React.ReactNode
 }
 
 const MainLayout =({children}: Props)=> {
     return (
-        <div>
-            {children}
+        <>
+        <MobileHeader/>
+        <Sidebar className="hidden lg:flex"/>
+            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="max-w-[1056px] mx-auto pt-6 h-full">
+          {children}
         </div>
+      </main>
+        </>
     )
 }
 
