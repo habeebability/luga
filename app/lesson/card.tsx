62930267b7b8f4084f8e9703478d6ee0a3e4ee48
Habeebability <awoyemi.habeeb@gmail.com>
2024-03-23 23:07:11 +0000

    add quiz question cards

---
+++ app/lesson/card.tsx
@@ -0,0 +1,54 @@
+import { challenges } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  id: number;
+  imageSrc: string | null;
+  audioSrc: string | null;
+  text: string;
+  shortcut: string;
+  selected?: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+  status: "correct" | "wrong" | "none";
+  type: (typeof challenges.$inferSelect)["type"];
+};
+
+export const Card = ({
+  id,
+  imageSrc,
+  audioSrc,
+  text,
+  shortcut,
+  selected,
+  onClick,
+  status,
+  disabled,
+  type,
+}: Props) => {
+  return (
+    <div
+      onClick={() => {}}
+      className={cn(
+        "h-full rounded-xl border-2 border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
+        selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
+        selected &&
+          status === "correct" &&
+          "border-purple-300 bg-purple-100 hover:bg-purple-100",
+        selected &&
+          status === "wrong" &&
+          "border-rose-100 bg-rose-100 hover:bg-rose-100",
+        disabled && "pointer-events-none hover:bg-white",
+        type === "ASSIST" && "lg:p-3 w-full"
+      )}
+    >
+      {imageSrc && (
+        <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
+          <Image src={imageSrc} fill alt={text} />
+        </div>
+      )}
+    </div>
+  );
+};
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -4,6 +4,7 @@ import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
 
 type Props = {
   initialPercentage: number;
@@ -35,6 +36,7 @@ export const Quiz = ({
   });
 
   const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
 
   const title =
     challenge.type === "ASSIST"
@@ -59,9 +61,17 @@ export const Quiz = ({
               {title}
             </h2>
             <div>
-              {challenge.type === "SELECT" && (
+              {challenge.type === "ASSIST" && (
                 <QuestionBubble question={challenge.question} />
               )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status="none"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
             </div>
           </div>
         </div>
