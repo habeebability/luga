d85afc0b9e90b32c5b14b618d9ef837706896107
Habeebability <awoyemi.habeeb@gmail.com>
2024-03-23 19:56:09 +0000

    add zustand, exit modal, learning page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,67 +1,68 @@
-import { FeedWrapper } from '@/components/feed-wrapper'
-import { StickyWrapper } from '@/components/sticky-wrapper'
-import React from 'react'
-import { Header } from './header'
-import { UserProgress } from '@/components/user-progress'
-import { getUnits, getUserProgress } from '@/db/queries'
-import { redirect } from 'next/navigation'
-import { Unit } from './unit'
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import React from "react";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/db/queries";
+import { redirect } from "next/navigation";
+import { Unit } from "./unit";
 
-const LearnPage = async() => {
+const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
   const unitsData = getUnits();
 
-  const [
-    userProgress,
-    units,
-  
-  ] = await Promise.all([
-    userProgressData,
-    unitsData
-   
-  ]);
+  const [userProgress, units, courseProgress, lessonPercentage] =
+    await Promise.all([
+      userProgressData,
+      unitsData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
+  if (!courseProgress) {
+    redirect("/courses");
+  }
 
   return (
-    <div className='flex flex-row-reverse gap-[48px] px-6'>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-        activeCourse={
-          userProgress.activeCourse
-        }
-        hearts={userProgress.hearts}
-        points={userProgress.points}
-        hasActiveSubscription={false}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
 
       <FeedWrapper>
-        <Header title={userProgress.activeCourse.title}/>
-      
-        {
-          units.map((unit)=> (
-            <div
-            
-            key={unit.id} className='mb-10'>
+        <Header title={userProgress.activeCourse.title} />
 
-              <Unit
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
               id={unit.id}
               order={unit.order}
               description={unit.description}
               title={unit.title}
               lessons={unit.lessons}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
-              />
-            </div>
-          ))
-        }
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default LearnPage
+export default LearnPage;
---
+++ components/modals/exit-modal.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { useExitModal } from "@/store/use-exit-modal";
+
+export const ExitModal = () => {
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = useExitModal();
+
+  useEffect(() => setIsClient(true), []);
+
+  if (!isClient) {
+    return null;
+  }
+};
