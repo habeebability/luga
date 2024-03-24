2701ef5b90b2b8c0f8921bd645b4749a0e6b6a5c
Habeebability <awoyemi.habeeb@gmail.com>
2024-03-24 00:51:15 +0000

    fix: courses list responsiveness

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { courses, userProgress } from "@/db/schema"
+import { courses, userProgress } from "@/db/schema";
 import { Card } from "./card";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
@@ -8,44 +8,39 @@ import { upsertUserProgress } from "@/actions/user-progress";
 import { toast } from "sonner";
 
 type Props = {
-    courses: typeof courses.$inferSelect[];
-    activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
-  }
+  courses: (typeof courses.$inferSelect)[];
+  activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
+};
 
-export const List = ({courses, activeCourseId}: Props)=> {
-  const router = useRouter()
-  const [pending, startTransition] = useTransition()
+export const List = ({ courses, activeCourseId }: Props) => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
 
-  const onClick = (id: number)=> {
-    if(pending) return;
+  const onClick = (id: number) => {
+    if (pending) return;
 
-    if(id=== activeCourseId){
-      return router.push("/learn")
+    if (id === activeCourseId) {
+      return router.push("/learn");
     }
 
-    startTransition(()=> {
-      upsertUserProgress(id)
-      .catch(()=>toast.error("Something went wrong"))
-    })
-  }
+    startTransition(() => {
+      upsertUserProgress(id).catch(() => toast.error("Something went wrong"));
+    });
+  };
 
-
-
-    return (
-        <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
-          {
-            courses.map((course)=> (
-                <Card
-                key={course.id}
-                id={course.id}
-                title={course.title}
-                imageSrc={course.imageSrc}
-                onClick={onClick}
-                disabled={pending}
-                active={course.id === activeCourseId}
-                />
-            ))
-          }
-        </div>
-    )
-}
+  return (
+    <div className="pt-6 grid grid-cols-1 md:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
+      {courses.map((course) => (
+        <Card
+          key={course.id}
+          id={course.id}
+          title={course.title}
+          imageSrc={course.imageSrc}
+          onClick={onClick}
+          disabled={pending}
+          active={course.id === activeCourseId}
+        />
+      ))}
+    </div>
+  );
+};
